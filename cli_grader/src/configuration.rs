bcc55a014180ed4b3b60d71b8a472d87bb969672
ArturAssisComp <arturassiscomp@gmail.com>
2025-08-17 16:34:46 -0300

    add tests for TestSection

--- cli_grader/src/configuration.rs
+++ cli_grader/src/configuration.rs
@@ -476,6 +476,16 @@ impl UnitTests {
             tests,
         })
     }
+
+    #[cfg(test)]
+    fn new_dummy() -> Self {
+        Self {
+            env: vec![("k1".to_string(), "v1".to_string())],
+            setup: vec!["s1".to_string(), "s2".to_string()],
+            teardown: vec![],
+            tests: vec![UnitTest::new_dummy(1), UnitTest::new_dummy(2)],
+        }
+    }
 }
 
 impl TryFrom<UnitTestsUnchecked> for UnitTests {
@@ -499,6 +509,17 @@ impl TryFrom<UnitTestsUnchecked> for UnitTests {
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[serde(deny_unknown_fields)]
+struct TestSectionUnchecked {
+    title: Option<String>,
+    weight: Option<u32>,
+    unit_tests: Option<UnitTests>,
+    // integration_tests: IntegrationTests,
+    // performance_tests: PerformanceTests,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[serde(try_from = "TestSectionUnchecked")]
 struct TestSection {
     title: Option<String>,
     weight: Option<u32>,
@@ -509,6 +530,38 @@ struct TestSection {
     unit_tests: Option<UnitTests>,
     // integration_tests: IntegrationTests,
     // performance_tests: PerformanceTests,
+}
+
+impl TestSection {
+    fn build(
+        title: Option<String>,
+        weight: Option<u32>,
+        unit_tests: Option<UnitTests>,
+    ) -> Result<Self, &'static str> {
+        if unit_tests.is_none() {
+            return Err("at least one type of test is expected in the TestSection");
+        }
+
+        Ok(Self {
+            title,
+            weight,
+            unit_tests,
+        })
+    }
+}
+
+impl TryFrom<TestSectionUnchecked> for TestSection {
+    type Error = &'static str;
+
+    fn try_from(value: TestSectionUnchecked) -> Result<Self, Self::Error> {
+        let TestSectionUnchecked {
+            title,
+            weight,
+            unit_tests,
+        } = value;
+
+        TestSection::build(title, weight, unit_tests)
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
@@ -1468,6 +1521,153 @@ mod tests {
             ]
         }"#,
             UnitTests
+        );
+    }
+
+    mod test_test_section {
+        use crate::configuration::{TestSection, UnitTests};
+
+        // serialization
+        test_serialize_and_deserialize!(
+            should_serialize_deserialize_full,
+            TestSection {
+                title: Some("section 1".to_string()),
+                weight: None,
+                unit_tests: Some(UnitTests::new_dummy()),
+            },
+            TestSection
+        );
+        // invalid deserialization
+        test_invalid_deserialization!(should_panic_with_no_content_string, r#"\n"#, TestSection);
+        test_invalid_deserialization!(should_panic_with_empty_object, r#"{}"#, TestSection);
+        test_invalid_deserialization!(
+            should_panic_with_wrong_field,
+            r#"
+        {
+            "wrong field":123
+        }"#,
+            TestSection
+        );
+        test_invalid_deserialization!(
+            should_panic_without_tests,
+            r#"
+        {
+            "title":"sec 1",
+            "weight": 2
+        }"#,
+            TestSection
+        );
+        test_invalid_deserialization!(
+            should_panic_with_extra_field,
+            r#"
+        {
+            "title":"sec 1",
+            "weight": 2,
+            "extra":"field",
+            "unit_tests":{
+                "setup":["cmd1 abc"],
+                "tests": [
+                    {
+                        "title":"name 1",
+                        "program_name":"main",
+                        "detailed_tests":[
+                            {
+                                "args":"a1 a2 a3",
+                                "name":"test 1",
+                                "status":0,
+                                "stdout":"hello world"
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+        "#,
+            TestSection
+        );
+        test_invalid_deserialization!(
+            should_panic_with_wrong_field_name,
+            r#"
+        {
+            "title":"sec 1",
+            "weigt": 2,
+            "unit_tests":{
+                "setup":["cmd1 abc"],
+                "tests": [
+                    {
+                        "title":"name 1",
+                        "program_name":"main",
+                        "detailed_tests":[
+                            {
+                                "args":"a1 a2 a3",
+                                "name":"test 1",
+                                "status":0,
+                                "stdout":"hello world"
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+        "#,
+            TestSection
+        );
+        test_invalid_deserialization!(
+            should_panic_with_duplicated_field,
+            r#"
+        {
+            "title":"sec 1",
+            "weight": 2,
+            "weight": 2,
+            "unit_tests":{
+                "setup":["cmd1 abc"],
+                "tests": [
+                    {
+                        "title":"name 1",
+                        "program_name":"main",
+                        "detailed_tests":[
+                            {
+                                "args":"a1 a2 a3",
+                                "name":"test 1",
+                                "status":0,
+                                "stdout":"hello world"
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+        "#,
+            TestSection
+        );
+
+        // valid deserialization
+        test_valid_deserialization!(
+            should_accept_valid_section,
+            r#"
+        {
+            "title":"sec 1",
+            "weight": 2,
+            "unit_tests":{
+                "setup":["cmd1 abc"],
+                "tests": [
+                    {
+                        "title":"name 1",
+                        "program_name":"main",
+                        "detailed_tests":[
+                            {
+                                "args":"a1 a2 a3",
+                                "name":"test 1",
+                                "status":0,
+                                "stdout":"hello world"
+                            }
+                        ]
+                    }
+                ]
+            }
+        }
+        "#,
+            TestSection
         );
     }
 
